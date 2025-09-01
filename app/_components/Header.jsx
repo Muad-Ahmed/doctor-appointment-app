1febddf9a4038ae587064677dcf8e987783642de
Muad-Ahmed <muad8010@gmail.com>
2025-09-01 16:12:47 +0300

    style: change global font

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between p-3 shadow-sm">
       <div className="flex items-center gap-10">
-        <Image src={"/assets/imgs/logo.png"} width={50} height={50} alt="img" />
+        <Image src={"/assets/imgs/logo.png"} width={100} height={100} alt="img" />
         <ul className="md:flex gap-8 hidden">
           {Menu.map((item, index) => (
             <Link href={item.path} key={item.name}>
