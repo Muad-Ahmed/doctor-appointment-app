129caed27247d02c48106ee5647b48f006a148de
Muad-Ahmed <muad8010@gmail.com>
2025-09-01 14:33:37 +0300

    feat: add Header component to layout

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const Menu = [
   {
     id: 1,
@@ -9,20 +11,31 @@ const Menu = [
   {
     id: 2,
     name: "Explore",
-    path: "/",
+    path: "/explore",
   },
   {
     id: 3,
     name: "Contact Us",
-    path: "/",
+    path: "/contact",
   },
 ];
 
 const Header = () => {
   return (
-    <div>
-      <Image src={"/assets/imgs/logo.png"} width={50} height={50} />
-      <h1>Hello</h1>
+    <div className="flex items-center justify-between p-3 shadow-sm">
+      <div className="flex items-center gap-10">
+        <Image src={"/assets/imgs/logo.png"} width={50} height={50} alt="img" />
+        <ul className="md:flex gap-8 hidden">
+          {Menu.map((item, index) => (
+            <Link href={item.path} key={item.name}>
+              <li className="hover:text-lime-600 cursor-pointer hover:scale-105 transition-all">
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <Button>Get Started</Button>
     </div>
   );
 };
